a478a0ff69f9b52491f81787600cc649895d3897
Ömer Yağmurlu <omeryagmurlu@gmail.com>
2024-12-09 16:31:57 +0100

    wip

--- manten/scripts/train.py
+++ manten/scripts/train.py
@@ -1,304 +1,17 @@
 import os
-from dataclasses import dataclass, field
 
 import hydra
 import torch
 from accelerate import Accelerator
 from accelerate.utils import DistributedDataParallelKwargs, set_seed
 from omegaconf import OmegaConf
-from tabulate import tabulate
 
-from manten.agents.base_agent import AgentMode
 from manten.utils.logging import get_logger
-from manten.utils.progbar import progbar
+from manten.utils.loops import Loops
 from manten.utils.root import root
-from manten.utils.utils_file import mkdir, write_json
-
-
-def nop(*_, **__):
-    return
-
+from manten.utils.utils_file import mkdir
 
 logger = get_logger(__name__)
-
-
-def loop_factory(
-    *,
-    compute_batch_result,
-    after_batch=nop,
-    progress_bar_impl=progbar,
-    progbar_kwargs=None,
-):
-    progbar_kwargs_top = progbar_kwargs
-    if progbar_kwargs_top is None:
-        progbar_kwargs_top = {}
-
-    def loop(
-        dataloader,
-        agent,
-        *,
-        max_steps=float("inf"),
-        log_aggregator=None,
-        progbar_kwargs=None,
-    ):
-        if progbar_kwargs is None:
-            progbar_kwargs = {}
-        progbar_kwargs = {**progbar_kwargs_top, **progbar_kwargs}
-
-        if log_aggregator:
-            log_aggregator.reset()
-        agent.eval()
-        for step, batch in enumerate(
-            progress := progress_bar_impl(
-                dataloader,
-                total=min(len(dataloader), max_steps),
-                **progbar_kwargs,
-            )
-        ):
-            if step == max_steps:
-                break
-            batch_result = compute_batch_result(
-                agent=agent,
-                batch=batch,
-                log_aggregator=log_aggregator,
-                step=step,
-                progress=progress,
-            )
-            after_batch(
-                agent=agent,
-                step=step,
-                batch=batch,
-                batch_result=batch_result,
-                log_aggregator=log_aggregator,
-                progress=progress,
-            )
-        progress.close()
-
-    return loop
-
-
-def do_agent_inference_validate(agent, batch, **_):
-    with torch.inference_mode():
-        metric = agent(AgentMode.VALIDATE, batch)
-    return metric
-
-
-def do_agent_inference_eval(agent, batch, **_):
-    with torch.inference_mode():
-        trajectory, metric = agent(AgentMode.EVAL, batch, compare_gt=True)
-    return trajectory, metric
-
-
-def update_progress(*, progress, metric, **_):
-    progress.set_postfix(**metric.summary_metrics())
-
-
-def update_progress_n_log(*, progress, metric, log_aggregator, **_):
-    progress.set_postfix(**metric.summary_metrics())
-    log_aggregator.log(metric)
-
-
-def sanity_check(dataloader, agent, sanity_check_steps=5):
-    validate_sanity_check = loop_factory(
-        compute_batch_result=do_agent_inference_validate,
-        after_batch=lambda batch_result, **ka: update_progress(metric=batch_result, **ka),
-        progbar_kwargs={"desc": "sanity check (validate)"},
-    )
-
-    eval_sanity_check = loop_factory(
-        compute_batch_result=do_agent_inference_eval,
-        after_batch=lambda batch_result, **ka: update_progress(metric=batch_result[1], **ka),
-        progbar_kwargs={"desc": "sanity check (eval)"},
-    )
-
-    validate_sanity_check(
-        dataloader,
-        agent,
-        max_steps=sanity_check_steps,
-    )
-    eval_sanity_check(
-        dataloader,
-        agent,
-        max_steps=sanity_check_steps,
-    )
-
-    logger.info("Sanity check successful")
-
-
-@dataclass
-class TrainState:
-    epoch: int = field(default=0)
-    global_step: int = field(default=0)
-    best_loss: float = field(default=float("inf"))
-
-    def state_dict(self):
-        return {
-            "epoch": self.epoch,
-            "global_step": self.global_step,
-            "best_loss": self.best_loss,
-        }
-
-    def load_state_dict(self, state_dict):
-        self.epoch = state_dict["epoch"]
-        self.global_step = state_dict["global_step"]
-        self.best_loss = state_dict["best_loss"]
-
-
-# TODO: This is apparently a very long and complex function, but it'll have to do for now
-def train(  # noqa: C901, PLR0915, PLR0912
-    cfg,
-    accelerator,
-    agent,
-    train_dl,
-    test_dl,
-    optimizer,
-    lr_scheduler,
-    log_aggregator,
-):
-    validate = loop_factory(
-        compute_batch_result=do_agent_inference_validate,
-        after_batch=lambda batch_result, **kwargs: update_progress_n_log(
-            metric=batch_result, **kwargs
-        ),
-        progbar_kwargs={"leave": False},
-    )
-    eeval = loop_factory(
-        compute_batch_result=do_agent_inference_eval,
-        after_batch=lambda batch_result, **kwargs: update_progress_n_log(
-            metric=batch_result[1], **kwargs
-        ),
-        progbar_kwargs={"leave": False},
-    )
-
-    def every_n_epochs(n):
-        return bool(n) and ((state.epoch + 1) % n == 0 or (state.epoch + 1) == cfg.num_epochs)
-
-    def every_n_global_steps(n):
-        return bool(n) and ((state.global_step + 1) % n == 0 or (step + 1) == len_train_dl)
-
-    def every_n_universe_steps(n):
-        return bool(n) and ( ((state.global_step + 1) * num_processes) % n < num_processes or (step + 1) == len_train_dl )
-
-    ism = accelerator.is_main_process
-    num_processes = accelerator.num_processes
-    len_train_dl = len(train_dl)
-
-    state = TrainState()
-    accelerator.register_for_checkpointing(state)
-
-    # Potentially load in the weights and states from a previous save
-    if cfg.resume_from_save:
-        # TODO: maybe later automatic loading of last/best checkpoint
-        checkpoint_to_load = cfg.resume_from_save
-
-        accelerator.load_state(checkpoint_to_load)
-        logger.info(
-            "resuming from checkpoint@epoch:%d via %s", state.epoch, checkpoint_to_load
-        )
-        state.epoch += 1  # to start from the next epoch instead of redoing the current one
-
-    logger.info("starting training")
-    start_epoch = state.epoch
-    for state.epoch in range(start_epoch, cfg.num_epochs):
-        log_aggregator.reset()
-        agent.train()
-        for step, batch in enumerate( # train_dl syncs rnd seed across processes
-            progress := progbar(train_dl, desc=f"epoch {state.epoch}")
-        ):
-            with accelerator.accumulate(agent):
-                metric = agent(AgentMode.TRAIN, batch)
-                loss = metric.loss()
-
-                accelerator.backward(loss)
-                if accelerator.sync_gradients:
-                    accelerator.clip_grad_norm_(agent.parameters(), 1.0)
-
-                optimizer.step()
-                lr_scheduler.step()
-                optimizer.zero_grad()
-
-            overview_logs = {
-                "loss": loss.detach().item(),
-                "lr": lr_scheduler.get_last_lr()[0],
-                "global_step": state.global_step,
-            }
-            progress.set_postfix(**overview_logs)
-
-            # accelerate already has @on_main_process deco on trackers, so this here
-            # is mostly redundant, it only prevents agent.metrics() from being called
-            # on non-main processes at all
-            if ism and every_n_universe_steps(cfg.log_every_n_steps):
-                upl = log_aggregator.log_collate(metric, "train/")
-                upl.update(overview_logs)
-                upl.update({
-                    "step": step,
-                    "epoch": state.epoch,
-                    "universe_step": state.global_step * num_processes,
-                })
-                accelerator.log(upl, step=state.global_step)
-
-            state.global_step += 1
-
-        if every_n_epochs(cfg.validate_every_n_epochs):
-            accelerator.wait_for_everyone()
-            if ism:
-                validate(
-                    test_dl,
-                    agent,
-                    max_steps=cfg.validate_ene_max_steps,
-                    progbar_kwargs={"desc": f"epoch {state.epoch} (validate)"},
-                    log_aggregator=log_aggregator,
-                )
-                logger.info("validate@epoch:%d", state.epoch)
-                accelerator.log(
-                    validate_logs := log_aggregator.collate("validate/"), step=state.global_step
-                )
-                print(tabulate(validate_logs.items()))
-
-        for every_n, dl, max_steps, split in [
-            # (cfg.eval_train_every_n_epochs, train_dl, cfg.eval_train_ene_max_steps, "train"),
-            (cfg.eval_test_every_n_epochs, test_dl, cfg.eval_test_ene_max_steps, "test"),
-        ]:
-            if every_n_epochs(every_n):
-                accelerator.wait_for_everyone()
-                if ism:
-                    eeval(
-                        dl,
-                        agent,
-                        max_steps=max_steps,
-                        progbar_kwargs={"desc": f"epoch {state.epoch} (eval:{split})"},
-                        log_aggregator=log_aggregator,
-                    )
-                    logger.info("evaluation:%s@epoch:%d", split, state.epoch)
-
-                    eval_logs = log_aggregator.collate(f"eval-{split}/", reset=False)
-                    print(tabulate(eval_logs.items()))
-                    eval_logs.update(log_aggregator.create_vis_logs("mae_pos"))
-                    accelerator.log(eval_logs, step=state.global_step)
-
-                    log_aggregator.reset()
-
-        if every_n_epochs(cfg.save_every_n_epochs):
-            accelerator.wait_for_everyone()
-            if ism:
-                checkpoint_path = f"{accelerator.project_dir}/checkpoint_{state.epoch}"
-                accelerator.save_state(checkpoint_path)
-                logger.info("checkpoint@epoch:%d saved to %s", state.epoch, checkpoint_path)
-
-                if overview_logs["loss"] < state.best_loss:
-                    state.best_loss = overview_logs["loss"]
-                    best_checkpoint_path = f"{accelerator.project_dir}/best_checkpoint"
-                    accelerator.save_state(best_checkpoint_path)
-                    write_json(
-                        f"{accelerator.project_dir}/best_checkpoint/overview_logs.json",
-                        overview_logs,
-                    )
-                    logger.info(
-                        "best checkpoint@epoch:%d with loss %.4f saved to %s",
-                        state.epoch,
-                        state.best_loss,
-                        best_checkpoint_path,
-                    )
 
 
 @hydra.main(version_base=None, config_path=str(root / "configs"), config_name="train")
@@ -306,8 +19,8 @@ def main(cfg):
     """
     Main function to train an agent.
     """
-    set_seed(cfg.training.seed, deterministic=cfg.training.deterministic)
-    if cfg.training.deterministic:
+    set_seed(cfg.seed, deterministic=cfg.deterministic)
+    if cfg.deterministic:
         # also need to handle this but nvm for now: https://docs.nvidia.com/cuda/cublas/index.html#results-reproducibility
         torch.backends.cudnn.benchmark = False
 
@@ -353,10 +66,18 @@ def main(cfg):
         agent, optimizer, train_dataloader, lr_scheduler
     )
 
-    log_aggregator = hydra.utils.instantiate(cfg.training.log_aggregator)
+    loops = Loops(
+        cfg.training,
+        accelerator=accelerator,
+        agent=agent,
+        train_dl=train_dataloader,
+        test_dl=test_dataloader,
+        optimizer=optimizer,
+        lr_scheduler=lr_scheduler,
+        log_aggregator=hydra.utils.instantiate(cfg.training.log_aggregator),
+    )
 
-    if bool(cfg.training.sanity_check):
-        sanity_check(test_dataloader, agent, cfg.training.sanity_check)
+    loops.begin_sanity_check()
 
     # accelerator already handles is_main_process for trackers
     init_dict = {**OmegaConf.to_container(cfg.accelerator_init_trackers, resolve=True)}
@@ -367,16 +88,7 @@ def main(cfg):
         )
     accelerator.init_trackers(**init_dict)
 
-    train(
-        cfg.training,
-        accelerator,
-        agent,
-        train_dataloader,
-        test_dataloader,
-        optimizer,
-        lr_scheduler,
-        log_aggregator,
-    )
+    loops.begin_training()
 
     # potentially online eval?
 
---
+++ manten/utils/loops.py
@@ -0,0 +1,306 @@
+from dataclasses import dataclass, field
+from typing import Protocol
+
+import tabulate
+import torch
+
+from manten.agents.base_agent import AgentMode
+from manten.utils.logging import get_logger
+from manten.utils.progbar import progbar
+
+logger = get_logger(__name__)
+
+
+@dataclass
+class LoopsState:
+    epoch: int = field(default=0)
+    global_step: int = field(default=0)
+    best_mean_train_epoch_loss: float = field(default=float("inf"))
+
+    def state_dict(self):
+        return {
+            "epoch": self.epoch,
+            "global_step": self.global_step,
+            "best_mean_train_epoch_loss": self.best_mean_train_epoch_loss,
+        }
+
+    def load_state_dict(self, state_dict):
+        self.epoch = state_dict["epoch"]
+        self.global_step = state_dict["global_step"]
+        self.best_mean_train_epoch_loss = state_dict["best_mean_train_epoch_loss"]
+
+
+class LoopsConfig(Protocol):
+    num_epochs: int
+    sanity_check: int
+    log_every_n_steps: int
+    save_every_n_epochs: int
+    resume_from_save: str | None
+    validate_every_n_epochs: int
+    eval_test_every_n_epochs: int
+    eval_train_every_n_epochs: int
+    validate_ene_max_steps: int
+    eval_test_ene_max_steps: int
+    eval_train_ene_max_steps: int
+
+
+class Loops:
+    def __init__(
+        self,
+        cfg: LoopsConfig,
+        *,
+        accelerator,
+        agent,
+        train_dl,
+        test_dl,
+        optimizer,
+        lr_scheduler,
+        log_aggregator,
+    ):
+        self.cfg = cfg
+        self.accelerator = accelerator
+        self.agent = agent
+        self.train_dl = train_dl
+        self.test_dl = test_dl
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
+        self.log_aggregator = log_aggregator
+
+        self.state = LoopsState()
+        accelerator.register_for_checkpointing(self.state)
+
+        self.num_processes = accelerator.num_processes
+        self.len_train_dl = len(train_dl)
+
+    def begin_training(self):
+        # Potentially load in the weights and states from a previous save
+        if self.cfg.resume_from_save:
+            self.resume_from_save(self.cfg.resume_from_save)
+
+        logger.info("starting training")
+        # loop control variable self overwrites as it iterates, but that's what we want
+        for self.state.epoch in range(self.state.epoch, self.cfg.num_epochs):  # noqa: B020
+            mean_epoch_loss, last_batch_loss = self.train_loop()
+
+            if self.every_n_epochs_wait_fe_on_ism(self.cfg.validate_every_n_epochs):
+                self.validate_loop()
+
+            # if self.every_n_epochs_wait_fe_on_ism(self.cfg.eval_train_every_n_epochs):
+            #     self.evaluation_loop(
+            #         self.train_dl, self.cfg.eval_train_ene_max_steps, "train"
+            #     )
+
+            if self.every_n_epochs_wait_fe_on_ism(self.cfg.eval_test_every_n_epochs):
+                self.evaluation_loop(self.test_dl, self.cfg.eval_test_ene_max_steps, "test")
+
+            if self.every_n_epochs_wait_fe_on_ism(self.cfg.save_every_n_epochs):
+                self.save_checkpoint(mean_epoch_loss)
+        logger.info("training finished, trained for %d epochs", self.state.epoch)
+
+    def begin_sanity_check(self):
+        logger.info("starting sanity check")
+        self.sanity_check_loop()
+        logger.info("sanity check successful")
+
+    def train_loop(self) -> tuple[float, float]:
+        self.log_aggregator.reset()
+        self.agent.train()
+        train_epoch_losses = []
+        for step, batch in enumerate(  # train_dl syncs rnd seed across processes
+            progress := progbar(self.train_dl, desc=f"epoch {self.state.epoch}")
+        ):
+            metric, loss = self.train_step(batch)
+
+            train_epoch_losses.append(loss := loss.detach().item())
+            overview_logs = {
+                "loss": loss,
+                "lr": self.lr_scheduler.get_last_lr()[0],
+                "timing/global_step": self.state.global_step,
+            }
+            progress.set_postfix(**overview_logs)
+
+            # TODO: for now just log main process metrics for performance reasons
+            # accelerate already has @on_main_process deco on trackers, so this here
+            # is mostly redundant, it only prevents agent.metrics() from being called
+            # on non-main processes at all
+            if self.accelerator.is_main_process and self.every_n_universe_steps(
+                self.cfg.log_every_n_steps
+            ):
+                upl = self.log_aggregator.log_collate(metric, "train/")
+                upl.update(overview_logs)
+                upl.update(
+                    {
+                        "timing/step": step,
+                        "timing/epoch": self.state.epoch,
+                        "timing/universe_step": self.state.global_step * self.num_processes,
+                    }
+                )
+                self.accelerator.log(upl, step=self.state.global_step)
+
+            self.state.global_step += 1
+
+        return sum(train_epoch_losses) / len(train_epoch_losses), loss
+
+    def validation_loop(self) -> None:
+        self.log_aggregator.reset()
+        self.agent.eval()
+        for step, batch in enumerate(
+            progress := progbar(
+                self.test_dl,
+                total=min(len(self.test_dl), self.cfg.validate_ene_max_steps),
+                desc=f"epoch {self.state.epoch} (validate)",
+                leave=False,
+            )
+        ):
+            if step == self.cfg.validate_ene_max_steps:
+                progress.close()
+                break
+
+            metric = self.validation_step(batch)
+
+            progress.set_postfix(**metric.summary_metrics())
+            self.log_aggregator.log(metric)
+
+        logger.info("validate@epoch:%d", self.state.epoch)
+        self.accelerator.log(
+            validate_logs := self.log_aggregator.collate("validate/"),
+            step=self.state.global_step,
+        )
+        print(tabulate(validate_logs.items()))
+
+    def evaluation_loop(self, dataloader, max_steps, split) -> None:
+        self.log_aggregator.reset()
+        self.agent.eval()
+        for step, batch in enumerate(
+            progress := progbar(
+                dataloader,
+                total=min(len(dataloader), max_steps),
+                desc=f"epoch {self.state.epoch} (eval:{split})",
+                leave=False,
+            )
+        ):
+            if step == max_steps:
+                progress.close()
+                break
+
+            metric, trajectory = self.evaluation_step(batch)
+
+            progress.set_postfix(**metric.summary_metrics())
+            self.log_aggregator.log(metric)
+
+        logger.info("evaluation:%s@epoch:%d", split, self.state.epoch)
+
+        eval_logs = self.log_aggregator.collate(f"eval-{split}/", reset=False)
+        print(tabulate(eval_logs.items()))
+        eval_logs.update(self.log_aggregator.create_vis_logs("mae_pos"))
+        self.accelerator.log(eval_logs, step=self.state.global_step)
+        self.log_aggregator.reset()
+
+    def sanity_check_loop(self):
+        self.agent.eval()
+        for step, batch in enumerate(
+            progress := progbar(
+                self.test_dl,
+                total=min(len(self.test_dl), self.cfg.sanity_check),
+                desc="sanity check (validate)",
+            )
+        ):
+            if step == self.cfg.sanity_check:
+                progress.close()
+                break
+
+            metric = self.validation_step(batch)
+            progress.set_postfix(**metric.summary_metrics())
+
+        self.agent.eval()
+        for step, batch in enumerate(
+            progress := progbar(
+                self.test_dl,
+                total=min(len(self.test_dl), self.cfg.sanity_check),
+                desc="sanity check (eval)",
+            )
+        ):
+            if step == self.cfg.sanity_check:
+                progress.close()
+                break
+
+            metric, trajectory = self.evaluation_step(batch)
+            progress.set_postfix(**metric.summary_metrics())
+
+    def train_step(self, batch):
+        with self.accelerator.accumulate(self.agent):
+            metric = self.agent(AgentMode.TRAIN, batch)
+            loss = metric.loss()
+
+            self.accelerator.backward(loss)
+            if self.accelerator.sync_gradients:
+                self.accelerator.clip_grad_norm_(self.agent.parameters(), 1.0)
+
+            self.optimizer.step()
+            self.lr_scheduler.step()
+            self.optimizer.zero_grad()
+
+        return metric, loss
+
+    def validation_step(self, batch):
+        with torch.inference_mode():
+            metric = self.agent(AgentMode.VALIDATE, batch)
+        return metric
+
+    def evaluation_step(self, batch):
+        with torch.inference_mode():
+            trajectory, metric = self.agent(AgentMode.EVAL, batch, compare_gt=True)
+        return metric, trajectory
+
+    def resume_from_save(self, from_str):
+        # TODO: maybe later automatic loading of last/best checkpoint
+        checkpoint_to_load = from_str
+
+        self.accelerator.load_state(checkpoint_to_load)
+        logger.info(
+            "resuming from checkpoint@epoch:%d via %s", self.state.epoch, checkpoint_to_load
+        )
+        self.state.epoch += (
+            1  # to start from the next epoch instead of redoing the current one
+        )
+
+    def save_checkpoint(self, mean_epoch_loss):
+        checkpoint_path = f"{self.accelerator.project_dir}/checkpoint_{self.state.epoch}"
+        self.accelerator.save_state(checkpoint_path)
+        logger.info("checkpoint@epoch:%d saved to %s", self.state.epoch, checkpoint_path)
+
+        if mean_epoch_loss < self.state.best_mean_train_epoch_loss:
+            self.state.best_mean_train_epoch_loss = mean_epoch_loss
+            best_checkpoint_path = f"{self.accelerator.project_dir}/best_checkpoint"
+            self.accelerator.save_state(best_checkpoint_path)
+            logger.info(
+                "best checkpoint@epoch:%d with loss %.4f saved to %s",
+                self.state.epoch,
+                self.state.best_mean_train_epoch_loss,
+                best_checkpoint_path,
+            )
+
+    def every_n_epochs(self, n):
+        return bool(n) and (
+            (self.state.epoch + 1) % n == 0
+            or (self.state.epoch + 1) == self.cfg.num_epochs  # or last epoch
+        )
+
+    def every_n_epochs_wait_fe_on_ism(self, n):
+        if not self.every_n_epochs(n):
+            return False
+
+        self.accelerator.wait_for_everyone()
+        return self.accelerator.is_main_process
+
+    def every_n_global_steps(self, n):
+        return bool(n) and (
+            (self.state.global_step + 1) % n == 0
+            or (self.step + 1) == self.len_train_dl  # or end of epoch
+        )
+
+    def every_n_universe_steps(self, n):
+        return bool(n) and (
+            ((self.state.global_step + 1) * self.num_processes) % n < self.num_processes
+            or (self.step + 1) == self.len_train_dl  # or end of epoch
+        )
