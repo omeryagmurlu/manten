ee62a2c19410aa625eec2d236e7adfd148dd9eba
Ömer Yağmurlu <omeryagmurlu@gmail.com>
2024-12-13 15:57:36 +0100

    manten_evaluation with agentproxy on mode works

---
+++ manten_evaluation/calvin/evaluate_policy.py
@@ -0,0 +1,252 @@
+# ruff: noqa: UP007
+
+import logging
+import sys
+import time
+from pathlib import Path
+from typing import Optional
+
+import torch
+
+from manten_evaluation.calvin.rollout_video import RolloutVideo
+
+sys.path.insert(0, Path(__file__).absolute().parents[2].as_posix())
+
+import einops
+import hydra
+import pyhash
+import torch.distributed as dist
+from calvin_agent.evaluation.multistep_sequences import get_sequences
+from calvin_agent.evaluation.utils import (
+    count_success,
+    get_env_state_for_initial_condition,
+    get_log_dir,
+    join_vis_lang,
+    print_and_save,
+)
+from calvin_env.envs.play_table_env import get_env
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import seed_everything
+from termcolor import colored
+from tqdm.auto import tqdm
+
+hasher = pyhash.fnv1_64()
+logger = logging.getLogger(__name__)
+
+
+def make_env(dataset_path):
+    val_folder = Path(dataset_path) / "validation"
+    env = get_env(val_folder, show_gui=False)
+
+    # env = Wrapper(env)
+    return env
+
+
+def get_video_tag(i):
+    if dist.is_available() and dist.is_initialized():
+        i = i * dist.get_world_size() + dist.get_rank()
+    return f"_long_horizon/sequence_{i}"
+
+
+def evaluate_policy(
+    model,
+    env,
+    calvin_models_dir,
+    epoch=42,
+    eval_log_dir=None,
+    debug=False,
+    num_sequences=1000,
+    ep_len=360,
+    num_videos=0,
+):
+    """
+    Run this function to evaluate a model on the CALVIN challenge.
+
+    Args:
+        model: Must implement methods of CalvinBaseModel.
+        env: (Wrapped) calvin env.
+        epoch:
+        eval_log_dir: Path where to log evaluation results. If None, logs to /tmp/evaluation/
+        debug: If True, show camera view and debug info.
+        create_plan_tsne: Collect data for TSNE plots of latent plans (does not work for your custom model)
+
+    Returns:
+        Dictionary with results
+    """
+    conf_dir = Path(calvin_models_dir) / "conf"
+    task_cfg = OmegaConf.load(conf_dir / "callbacks/rollout/tasks/new_playtable_tasks.yaml")
+    task_oracle = hydra.utils.instantiate(task_cfg)
+    val_annotations = OmegaConf.load(conf_dir / "annotations/new_playtable_validation.yaml")
+
+    if num_videos > 0:
+        rollout_video = RolloutVideo(
+            logger=logger, empty_cache=False, log_to_file=True, save_dir=eval_log_dir
+        )
+    else:
+        rollout_video = None
+
+    eval_log_dir = get_log_dir(eval_log_dir)
+
+    eval_sequences = get_sequences(num_sequences)
+
+    results = []
+
+    if not debug:
+        eval_sequences = tqdm(eval_sequences, position=0, leave=True)
+
+    for seq_idx, (initial_state, eval_sequence) in enumerate(eval_sequences):
+        if seq_idx < num_videos:
+            caption = " -> ".join(eval_sequence)
+            rollout_video.new_video(tag=get_video_tag(seq_idx), caption=caption)
+        result = evaluate_sequence(
+            env=env,
+            model=model,
+            task_checker=task_oracle,
+            initial_state=initial_state,
+            eval_sequence=eval_sequence,
+            val_annotations=val_annotations,
+            debug=debug,
+            ep_len=ep_len,
+            rollout_video=rollout_video if seq_idx < num_videos else None,
+        )
+        results.append(result)
+        if seq_idx < num_videos:
+            rollout_video.write_to_tmp()
+        if not debug:
+            eval_sequences.set_description(
+                " ".join(
+                    [
+                        f"{i + 1}/5 : {v * 100:.1f}% |"
+                        for i, v in enumerate(count_success(results))
+                    ]
+                )
+                + "|"
+            )
+
+    if num_videos > 0:
+        rollout_video._log_videos_to_file(0)  # noqa: SLF001
+    print_and_save(results, eval_sequences, eval_log_dir, epoch)
+
+    return results
+
+
+def evaluate_sequence(
+    env,
+    model,
+    task_checker,
+    initial_state,
+    eval_sequence,
+    val_annotations,
+    debug,
+    ep_len,
+    rollout_video: Optional[RolloutVideo],
+):
+    """
+    Evaluates a sequence of language instructions.
+    """
+    robot_obs, scene_obs = get_env_state_for_initial_condition(initial_state)
+    env.reset(robot_obs=robot_obs, scene_obs=scene_obs)
+
+    success_counter = 0
+    if debug:
+        time.sleep(1)
+        print()
+        print()
+        print(f"Evaluating sequence: {' -> '.join(eval_sequence)}")
+        print("Subtask: ", end="")
+    for subtask in eval_sequence:
+        if rollout_video is not None:
+            rollout_video.new_subtask()
+        success = rollout(
+            env=env,
+            model=model,
+            task_oracle=task_checker,
+            subtask=subtask,
+            val_annotations=val_annotations,
+            debug=debug,
+            ep_len=ep_len,
+            rollout_video=rollout_video,
+        )
+        if rollout_video is not None:
+            rollout_video.draw_outcome(success)
+        if success:
+            success_counter += 1
+        else:
+            return success_counter
+    return success_counter
+
+
+def rollout(
+    env,
+    model,
+    task_oracle,
+    subtask,
+    val_annotations,
+    debug,
+    ep_len,
+    rollout_video: Optional[RolloutVideo],
+):
+    """
+    Run the actual rollout on one subtask (which is one natural language instruction).
+    """
+    if debug:
+        print(f"{subtask} ", end="")
+        time.sleep(0.5)
+    obs = env.get_obs()
+    # get lang annotation for subtask
+    lang_annotation = val_annotations[subtask][0]
+    model.reset()
+    start_info = env.get_info()
+
+    for _step in range(ep_len):
+        action = model.step(obs, lang_annotation)
+        obs, _, _, current_info = env.step(action)
+        if debug:
+            img = env.render(mode="rgb_array")
+            join_vis_lang(img, lang_annotation)
+            # time.sleep(0.1)
+        if rollout_video is not None:
+            rollout_video.update(
+                einops.rearrange(
+                    torch.from_numpy(obs["rgb_obs"]["rgb_static"]), "h w c -> 1 1 c h w"
+                )
+            )
+
+        # check if current step solves a task
+        current_task_info = task_oracle.get_task_info_for_set(
+            start_info, current_info, {subtask}
+        )
+        if len(current_task_info) > 0:
+            if debug:
+                print(colored("success", "green"), end=" ")
+            if rollout_video is not None:
+                rollout_video.add_language_instruction(lang_annotation)
+            return True
+    if debug:
+        print(colored("fail", "red"), end=" ")
+    if rollout_video is not None:
+        rollout_video.add_language_instruction(lang_annotation)
+    return False
+
+
+@hydra.main(config_path="configs", config_name="evaluate_policy")
+def main(cfg: DictConfig):
+    seed_everything(0, workers=True)
+
+    model = hydra.utils.instantiate(cfg.agent_proxy)
+
+    env = make_env(cfg.dataset_path)
+    evaluate_policy(
+        model,
+        env,
+        calvin_models_dir=cfg.calvin_models_dir,
+        debug=cfg.debug,
+        eval_log_dir=cfg.eval_log_dir,
+        num_sequences=cfg.num_sequences,
+        ep_len=cfg.ep_len,
+        num_videos=cfg.num_videos,
+    )
+
+
+if __name__ == "__main__":
+    main()
--- manten_evaluation/cbackup/agent_proxies/tdda.py
+++ manten_evaluation/cbackup/agent_proxies/tdda.py
@@ -2,25 +2,8 @@ import numpy as np
 import torch
 import transformers
 
-from calvin.calvin_models.calvin_agent.models.calvin_base_model import CalvinBaseModel
 from manten.utils.dda_utils_with_calvin import relative_to_absolute
-from manten_evaluation.agent_proxy_client import AgentProxyClient
 from manten_evaluation.calvin.online_evaluation_calvin.evaluate_utils import convert_action
-
-
-def create_model(**kwargs):
-    return MantenCalvinAgentProxyClient(
-        agent_proxy_cfg=kwargs["agent_proxy_cfg"],
-        te_device=kwargs["te_device"],
-        interpolation_length=kwargs["interpolation_length"],
-        action_dim=kwargs["action_dim"],
-        num_history=kwargs["num_history"],
-        input_mode=kwargs["input_mode"],
-        relative_action=kwargs["relative_action"],
-        calvin_gripper_loc_bounds=kwargs["calvin_gripper_loc_bounds"],
-        text_encoder=kwargs["text_encoder"],
-        text_max_length=kwargs["text_max_length"],
-    )
 
 
 def get_text_encoder(text_encoder, text_max_length):
@@ -56,51 +39,33 @@ def get_text_encoder(text_encoder, text_max_length):
     return tokenizer, model
 
 
-class MantenCalvinAgentProxyClient(CalvinBaseModel):
+class TDDACalvinAdapter:
     def __init__(
         self,
-        *,
-        agent_proxy_cfg,  # noqa: ARG002
-        te_device,
         interpolation_length,
         action_dim,
         num_history,
-        input_mode,
+        te_device,
         relative_action,
         calvin_gripper_loc_bounds,
         text_encoder,
         text_max_length,
     ):
-        self.te_device = te_device
         self.interpolation_length = interpolation_length
         self.action_dim = action_dim
         self.num_history = num_history
-        self.input_mode = input_mode
+
+        self.te_device = te_device
         self.relative_action = relative_action
         self.calvin_gripper_loc_bounds = calvin_gripper_loc_bounds
         self.text_tokenizer, self.text_model = get_text_encoder(text_encoder, text_max_length)
 
-        # if agent_proxy_cfg is not None:
-        #     self._agent = AgentProxyClient(
-        #         address=tuple(agent_proxy_cfg.address),
-        #         authkey=bytes(str(agent_proxy_cfg.authkey), "utf-8"),
-        #     )
-        # else:
-        self._agent = AgentProxyClient()
-        self.reset()
-
     def reset(self):
-        """
-        This is called
-        """
-        self._agent.reset()
         self.text_model.eval()
         self.text_model = self.text_model.to(self.te_device)
 
     def encode_instruction(self, instruction, device=None):
         """Encode string instruction to latent embeddings.
-
-
 
         Args:
             instruction: a string of instruction
@@ -121,19 +86,7 @@ class MantenCalvinAgentProxyClient(CalvinBaseModel):
 
         return pred
 
-    def step(self, obs, instruction):
-        """
-        Args:
-            obs: a dictionary of observations
-                - rgb_obs: a dictionary of RGB images
-                - depth_obs: a dictionary of depth images
-                - robot_obs: a dictionary of proprioceptive states
-            lang_annotation: a string indicates the instruction of the task
-
-        Returns:
-            action: predicted action
-        """
-
+    def pre_step(self, obs, instruction):
         # Organize inputs
         trajectory_mask = torch.full([1, self.interpolation_length - 1], fill_value=False)
         fake_trajectory = torch.full([1, self.interpolation_length - 1, self.action_dim], 0)
@@ -173,7 +126,11 @@ class MantenCalvinAgentProxyClient(CalvinBaseModel):
             # has_3d=has_3d,
         }
 
-        _metric, trajectory = self._agent("eval", batch)
+        return ("eval", batch), (gripper,)
+
+    def post_step(self, agent_output, pre_step_output):
+        _metric, trajectory = agent_output
+        (gripper,) = pre_step_output
 
         # Convert quaternion to Euler angles
         trajectory = convert_action(trajectory)
@@ -191,5 +148,3 @@ class MantenCalvinAgentProxyClient(CalvinBaseModel):
                 a_min=self.calvin_gripper_loc_bounds[0].reshape(1, 1, 3),
                 a_max=self.calvin_gripper_loc_bounds[1].reshape(1, 1, 3),
             )
-
-        return trajectory
