7a7711c17f7c2c9adf59d742eab95b97e04a0911
Ömer Yağmurlu <omeryagmurlu@gmail.com>
2025-02-09 01:33:44 +0100

    latest-wip

--- manten_evaluation/maniskill2/download_and_replay_maniskill2_debug.sh
+++ manten_evaluation/maniskill2/download_and_replay_maniskill2_debug.sh
@@ -7,27 +7,27 @@ mkdir -p data/maniskill2
 cd data/maniskill2 || exit
 mkdir demos
 
-# echo "Downloading demonstrations to $(pwd)/demos"
-# echo "Downloading assets to $(pwd)/data"
-# for rigid_task in {"PickCube-v1",}
-# do
-# python -m mani_skill.utils.download_demo -o ./demos/ "$rigid_task"
-# python -m mani_skill.utils.download_asset "$rigid_task" -y
-# done
+echo "Downloading demonstrations to $(pwd)/demos"
+echo "Downloading assets to $(pwd)/data"
+for rigid_task in {"PegInsertionSide-v1",}
+do
+python -m mani_skill.utils.download_demo -o ./demos/ "$rigid_task"
+python -m mani_skill.utils.download_asset "$rigid_task" -y
+done
 
 
 # 3) replay demonstrations
 # reference: https://maniskill.readthedocs.io/en/latest/user_guide/datasets/replay.html
 # we use `pd_ee_delta_pose` as the target control mode by default
 echo "Replaying demonstrations..."
-for rigid_task in {"PickCube-v1",} # PickCube-v1, PegInsertionSide-v1
+for rigid_task in {"PegInsertionSide-v1",} # PickCube-v1, PegInsertionSide-v1
 do
 for obs_mode in {"state","pointcloud"} # rgbd, state, pointcloud
 do
 python -m mani_skill.trajectory.replay_trajectory \
   --traj-path "demos/$rigid_task/motionplanning/trajectory.h5" \
-  --save-traj --target-control-mode pd_ee_delta_pose --obs-mode "$obs_mode" --num-procs 20
-  # --count 45
+  --save-traj --target-control-mode pd_ee_delta_pose --obs-mode "$obs_mode" --num-procs 15 \
+  --count 30
   # --use-first-env-state \
 done
 done
