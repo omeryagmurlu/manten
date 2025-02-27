dc27fc5c153f55658570f8688b00114ac88c7cfd
Omer Erdinc Yagmurlu <omeryagmurlu@gmail.com>
2025-02-27 21:56:04 +0000

    remove tcp_pose from obs

--- manten/agents/combined_diffusion_policy/diff_t_combined_policy.py
+++ manten/agents/combined_diffusion_policy/diff_t_combined_policy.py
@@ -22,8 +22,13 @@ def noop_encoder(x, **_kwargs):
     return x
 
 
-def cat_all_encoder(x, **_kwargs):
-    return torch.cat(list(x.values()), dim=-1)
+# def cat_all_encoder(x, **_kwargs):
+#     return torch.cat(list(x.values()), dim=-1)
+
+def cat_all_encoder(x, excluded_keys=(), **_kwargs):
+    return torch.cat(
+        [x[key] for key in x if key not in excluded_keys], dim=-1
+    )
 
 
 @BatchPCDOrRGBObservationActionAgentTemplate.make_agent(
